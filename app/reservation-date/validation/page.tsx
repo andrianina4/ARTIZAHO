402988fc29d0c2ccc7a3057f24c536b266db6dd4
ROBUSTE Manohisafidy <robustemmanuel@gmail.com>
2023-12-12 11:32:53 +0300

    update : validation reservation page

--- app/reservation-date/validation/page.tsx
+++ app/reservation-date/validation/page.tsx
@@ -3,7 +3,7 @@ import Input from '@/components/input'
 import NavBar from '@/components/nav-bar'
 import PresentationTrainer from '@/components/presentation-trainer'
 import ShoppingCart from '@/components/shopping-cart-item'
-import { ArrowLeft, Phone, Place, RoundPlace } from '@/constants/link/icons'
+import { ArrowLeft, EyeOffIcon, Phone, Place, RoundPlace } from '@/constants/link/icons'
 import { trainers } from '@/data/temp/trainers'
 import Image from 'next/image'
 import React from 'react'
@@ -12,6 +12,7 @@ type Props = {}
 
 function ValidationDate({ }: Props) {
     const trainer = trainers[0]
+    const isConnected = true
 
     return (
         <div className='min-h-screen'>
@@ -38,11 +39,11 @@ function ValidationDate({ }: Props) {
 
 
                     <div className='flex-1 relative'>
-                        <div className='relative  h-[400px]'>
+                        <div className={`relative  h-[400px]  ${!isConnected ? 'blur-sm' : ''}`}>
                             <Image src="/temp/map.png" alt='map' fill className='object-cover rounded-2xl' />
                         </div>
 
-                        <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+                        <div className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  ${!isConnected ? 'blur-sm' : ''}`}>
                             <div className='bg-white px-5 py-2 rounded-md ml-8 -mb-5'>
                                 <h3 className='font-semibold'>Bouquet en vase</h3>
                                 <p className='flex text-brown items-center'><span className='text-2xl'><Place /></span> Antananarivo</p>
@@ -50,6 +51,10 @@ function ValidationDate({ }: Props) {
                             <span className='text-[#C26143] text-6xl'><RoundPlace /></span>
 
                         </div>
+                        {!isConnected ? <p className=' flex justify-center items-center gap-x-3 absolute top-1/2 left-0 right-0  transform text-center -translate-y-1/2 text-black-40%'>
+                            <span className='text-2xl'><EyeOffIcon /> </span>
+                            Veuillez vous connecter pour voir le contenu
+                        </p> : null}
                     </div>
 
                 </div>
