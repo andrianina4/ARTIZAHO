5fbca49865dc4fe52e35e420e5842cf7754ee9e9
Antonio <antonioheryramamonjisoa@gmail.com>
2024-01-24 16:06:01 +0300

    test: calendar

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,18 @@
+'use client'
 
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from "@fullcalendar/interaction"
 
-export default function Test() {
+  const handleDateClick = (arg) => { // bind with an arrow function
+  alert(arg.dateStr)}
+export default function Test() {  
+
   return <div className=" h-screen">
-   
+    <FullCalendar
+      plugins={[dayGridPlugin, interactionPlugin]}
+      dateClick={handleDateClick}
+      
+    />
   </div>;
 }
