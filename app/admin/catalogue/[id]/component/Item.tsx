8f3cf3b306206ad8a246f48b60ae99a26078f3a8
Onja <onjanirina1808@gmail.com>
2024-01-25 15:06:54 +0300

    Profil Atelier v2

--- app/admin/catalogue/[id]/component/Item.tsx
+++ app/admin/catalogue/[id]/component/Item.tsx
@@ -2,25 +2,26 @@ import React, {ReactNode} from "react";
 
 export default function Item({
 	label,
-	name,
+	value,
 	rightIcon,
 	image,
+	desc = false,
 }: {
 	label: string;
-	name: string;
+	value: string | number;
 	rightIcon?: ReactNode;
 	image?: string;
+	desc?: boolean;
 }) {
-	let styleBox = "w-4/5 h-16 bg-white-40% flex items-center px-6 rounded-2xl gap-2";
+	let styleBox = "w-4/5 bg-white-40% flex items-center px-6 rounded-2xl gap-2";
 	image ? (styleBox = styleBox + " font-bold") : null;
+	desc ? (styleBox = styleBox + " h-auto p-5") : (styleBox = styleBox + " h-16");
 	return (
 		<div className="flex flex-row">
-			<div className="w-1/5 flex items-center opacity-60">{label}</div>
-			<div className="w-4/5 h-16 bg-white-40% flex items-center px-6 rounded-2xl">
-				<div className={styleBox}>
-					{image ? <img src={image} className="w-9 h-9 rounded-full bg-black" /> : null}
-					{name}
-				</div>
+			<div className="w-1/5 flex items-center opacity-60 font-bold">{label}</div>
+			<div className={styleBox}>
+				{image ? <img src={image} className="w-9 h-9 rounded-full bg-black" /> : null}
+				{value}
 			</div>
 			<div className="flex items-center justify-center w-14">{rightIcon}</div>
 		</div>
--- app/admin/catalogue/[id]/page.tsx
+++ app/admin/catalogue/[id]/page.tsx
@@ -7,6 +7,12 @@ const atelier = {
 	id: 1,
 	name: "Bouquet en vase",
 	Artisan: {id: 1, name: "Mahefa", image: "/aze.png"},
+	localisation: "Antananarivo",
+	tarifs: 15,
+	heure_debut: "14:30",
+	duree: "1:30",
+	nb_max_participants: 10,
+	desc: "Welcome to Hanta's atelier-boutique, where you can learn how to create exceptional floral arrangements. During this experience, you'll discover a variety of foliage and flowers, and learn how to use them to create beautiful bouquets.",
 };
 
 export default function page({params}: {params: {id: string}}) {
@@ -19,7 +25,7 @@ export default function page({params}: {params: {id: string}}) {
 					leftIcon={<LeftLine />}
 					className="!w-6 !h-6 !bg-transparent !text-brown font-bold "
 				/>
-				<span className="text-2xl font-semibold text-brown-80%">Workshops List{params.id}</span>
+				<span className="text-2xl font-semibold text-brown-80%">Workshops List {params.id}</span>
 			</div>
 			<div className="flex flex-row w-full pl-12">
 				<div className="flex flex-col w-3/5">
@@ -27,15 +33,29 @@ export default function page({params}: {params: {id: string}}) {
 					<div className="flex flex-col gap-2">
 						<Item
 							label="Nom"
-							name={atelier.name}
+							value={atelier.name}
 							rightIcon={<EditFill className="w-6 h-6 opacity-50 ml-6" />}
 						/>
-						<Item label="Artisan" name={atelier.Artisan.name} image={atelier.Artisan.image} />
+						<Item label="Artisan" value={atelier.Artisan.name} image={atelier.Artisan.image} />
+						<Item label="Localisation" value={atelier.localisation} />
+						<Item label="Tarifs" value={atelier.tarifs} />
+						<div className="flex flex-row">
+							<div className="w-1/5 flex items-center opacity-60 font-bold"></div>
+							<div className="w-1/5 h-16 bg-white-40% flex items-center px-6 rounded-2xl">
+								{atelier.heure_debut}
+							</div>
+							<div className="w-1/5 h-16 flex items-center">Duration</div>
+							<div className="w-1/5 h-16 bg-white-40% flex items-center px-6 rounded-2xl">
+								{atelier.heure_debut}
+							</div>
+						</div>
+						<Item label="Participants" value={atelier.nb_max_participants} />
+						<Item label="Description" value={atelier.desc} desc />
 						<div className="flex flex-row"></div>
 					</div>
 				</div>
 				<div className="w-2/5 pl-7">
-					<div className="text-brown mb-5 font-bold">workshop performance</div>
+					<div className="text-brown mb-5 font-bold">Workshop performance</div>
 				</div>
 			</div>
 		</div>
