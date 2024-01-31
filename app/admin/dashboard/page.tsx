39d2047b75d2dbba23999b5c2c27c4d5d496efaa
Onja <onjanirina1808@gmail.com>
2024-01-31 08:30:34 +0300

    update

---
+++ app/admin/dashboard/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import AdminDashboard from "../content/AdminDashboard";
+import EntrepriseDashboard from "../content/EntrepriseDashboard";
+import {useState} from "react";
+
+export default function Page() {
+	const [isAdminDash, setDash] = useState(true);
+
+	return isAdminDash ? <AdminDashboard /> : <EntrepriseDashboard />;
+}
