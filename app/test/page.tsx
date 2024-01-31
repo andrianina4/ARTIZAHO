6fb847d8cf354a1ad5f94c1bfcb31d8d9bd7ccd8
Antonio <antonioheryramamonjisoa@gmail.com>
2024-01-31 07:59:16 +0300

    update: star score

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,18 +1,35 @@
-'use client'
+"use client"
 
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from "@fullcalendar/interaction"
+import React from 'react'
+import { useState } from 'react';
 
-  const handleDateClick = (arg:any) => { // bind with an arrow function
-  alert(arg.dateStr)}
-export default function Test() {  
+type Props = {}
 
-  return <div className=" h-screen">
-    <FullCalendar
-      plugins={[dayGridPlugin, interactionPlugin]}
-      dateClick={handleDateClick}
-      
-    />
-  </div>;
+function page({}: Props) {
+
+
+  const [rating, setRating] = useState(4); // Mettez ici la valeur de la note
+
+  const handleRatingChange = (value:any) => {
+    setRating(value);
+  };  
+  return (
+    <div className='flex items-center gap-6'>
+    <div className="rating rating-half rating-sm">
+      {[...Array(10)].map((_, index) => (
+        <input
+          key={index}
+          type="radio"
+          name="rating-10"
+          className={`bg-yellow-500 mask mask-star-2 mask-half-${(index % 2) + 1}`}
+          checked={index + 1 === rating*2}
+         
+        />
+      ))}
+    </div>
+    <div className='text-gray-60%'>{rating}/5</div>
+  </div>
+  )
 }
+
+export default page
