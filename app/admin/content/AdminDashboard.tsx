a645641691e2ed84a31ee637feeb4f2e86224066
Onja <onjanirina1808@gmail.com>
2024-02-02 15:01:39 +0300

    update : new dashboard

--- app/admin/content/AdminDashboard.tsx
+++ app/admin/content/AdminDashboard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {ChartSales, EyeFill, Group} from "@/constants/link/icons";
+import {ChartSales, EyeFill, Group, PeopleFill} from "@/constants/link/icons";
 import Bloc from "../component/bloc";
 import BlocBrown from "../component/blocBrown";
 import Notes from "./component/Notes";
@@ -10,20 +10,28 @@ import Graph from "./component/Graph";
 
 export default function AdminDashboard() {
 	return (
-		<div className="flex flex-col gap-5 h-full">
-			<div className="w-[800px] bg-white p-2 rounded-3xl">
-				<Graph />
+		<div className="flex flex-row gap-5 h-full">
+			<div className="flex flex-col gap-3">
+				<div className="w-[800px] bg-white p-6 rounded-3xl">
+					<Graph />
+				</div>
+				<div className="flex gap-5">
+					<Notes />
+					<div className="flex flex-col w-full gap-5">
+						<BlocBrown icon={<ChartSales />} textTop="Sales figures" textBottom="250" />
+						<Bloc icon={<PeopleFill />} textTop="Booking total" textBottom="250" brownIcon />
+					</div>
+				</div>
 			</div>
-			<div className="grid grid-cols-3 gap-10">
-				<Bloc icon={<Group />} textTop="Date" textBottom="10 Nov 2023" brownIcon />
-				<Bloc icon={<EyeFill />} textTop="Booking total" textBottom="250" brownIcon />
-				<BlocBrown icon={<ChartSales />} textTop="Sales figures" textBottom="250" />
-			</div>
-			<div id="Section 2" className="flex flex-row gap-5 w-full">
-				<Notes />
+			<div>
 				<NewAccount />
-				<Top />
 			</div>
+			{/* <Bloc icon={<Group />} textTop="Date" textBottom="10 Nov 2023" brownIcon /> */}
+			{/* <div className="grid grid-cols-3 gap-10"> */}
+			{/* </div> */}
+			{/* <div id="Section 2" className="flex flex-row gap-5 w-full"> */}
+			{/* <Top /> */}
+			{/* </div> */}
 			{/* <div id="Section 3" className="flex flex-col gap-5 w-full">
 				<Reservation />
 			</div> */}
--- app/admin/content/component/Graph.tsx
+++ app/admin/content/component/Graph.tsx
@@ -13,6 +13,20 @@ import {Bar} from "react-chartjs-2";
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 ChartJS.defaults.elements.bar.borderWidth = 2;
 
+// plugin chartJS
+const bgWhite = {
+	id: "bgWhite",
+	beforeDatasetsDraw(chart: any, args: any, plugins: any) {
+		const {
+			ctx,
+			chartArea: {top, bottom, left, right, width, height},
+		} = chart;
+		ctx.save();
+		ctx.fillstyle = "rgb(255,255,255)";
+		ctx.fillRect(left, top, width, height);
+	},
+};
+
 export const options = {
 	responsive: true,
 	plugins: {
@@ -24,6 +38,24 @@ export const options = {
 			text: "Number of bookings",
 		},
 	},
+	scales: {
+		x: {
+			beginAtZero: false,
+			grid: {
+				display: false,
+				drawOnChartArea: false,
+				drawBorder: false,
+			},
+		},
+		y: {
+			beginAtZero: false,
+			grid: {
+				display: false,
+				drawOnChartArea: false,
+				drawBorder: false,
+			},
+		},
+	},
 };
 
 const labels = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
@@ -33,14 +65,14 @@ export const data = {
 	datasets: [
 		{
 			label: "Company",
-			data: [65, 59, 80, 81, 56, 55, 40, 54, 26, 84, 21, 62],
-			backgroundColor: "#ECA853",
+			data: [50, 15, 26, 33, 45, 41, 21, 49, 39, 42, 19, 27],
+			backgroundColor: "#917155",
 			borderRadius: 14,
 		},
 		{
 			label: "Individuals",
-			data: [65, 59, 80, 81, 56, 55, 40, 54, 26, 84, 21, 62],
-			backgroundColor: "#70A6E8",
+			data: [46, 49, 24, 36, 37, 15, 22, 34, 14, 26, 41, 44],
+			backgroundColor: "#D7B697",
 			borderRadius: 14,
 		},
 	],
