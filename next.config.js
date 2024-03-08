9682f6c42bc6deb79057fecd0a8122df8453c402
Onja <onjanirina1808@gmail.com>
2024-03-08 09:48:15 +0300

    fix bug

--- next.config.js
+++ next.config.js
@@ -1,51 +1,49 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config, { isServer }) {
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.(".svg")
-    );
+	webpack(config, {isServer}) {
+		const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.(".svg"));
 
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-        use: ["@svgr/webpack"],
-      }
-    );
-    fileLoaderRule.exclude = /\.svg$/i;
+		config.module.rules.push(
+			{
+				...fileLoaderRule,
+				test: /\.svg$/i,
+				resourceQuery: /url/, // *.svg?url
+			},
+			{
+				test: /\.svg$/i,
+				issuer: fileLoaderRule.issuer,
+				resourceQuery: {not: [...fileLoaderRule.resourceQuery.not, /url/]}, // exclude if *.svg?url
+				use: ["@svgr/webpack"],
+			}
+		);
+		fileLoaderRule.exclude = /\.svg$/i;
 
-    return config;
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "9237",
-        pathname: "/**",
-      },
-    ],
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/image/:path*",
-        destination: `${process.env.BACKEND_URL}/:path*`, // Proxy to Backend image
-      },
-    ];
-  },
+		return config;
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "images.unsplash.com",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "http",
+				hostname: "localhost",
+				port: "9237",
+				pathname: "/**",
+			},
+		],
+	},
+	async rewrites() {
+		return [
+			{
+				source: "/image/:path*",
+				destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/:path*`, // Proxy to Backend image
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
