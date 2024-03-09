5d06e81e21ad88e63c86fd3c6ace4f337611ce0c
Andrianina <araherimanantsoa@bocasay.com>
2024-03-09 20:58:33 +0300

    Mise à jours ts craftman

--- types/ICraftman.ts
+++ types/ICraftman.ts
@@ -1,21 +1,24 @@
-import {ReactNode} from "react";
+import { ReactNode } from "react";
+import { IImage } from "./IImage";
 
-export type Gender="M"|"F"|"O"
+export type Gender = "M" | "F" | "O";
 export type ICraftman = {
-	name: string;
-	description: string;
-	expertise: "Floral";
-	gender: Gender
+  id: number;
+  images: IImage[];
+  availabilities: [];
+  name: string;
+  description: string;
+  expertise: "Floral";
+  gender: Gender;
 };
 
 export type ICraftmanItem = {
-	name: string;
-	image: string;
-	expertise: string
-	score:"5"
-	nbre_clients:"40"
+  name: string;
+  image: string;
+  expertise: string;
+  score: "5";
+  nbre_clients: "40";
 };
-
 
 // export type ICraftmanItem = {
 // 	craftman_name: string;
@@ -30,8 +33,8 @@ export type ICraftmanItem = {
 // };
 
 export type ISuggestCraftman = {
-	craftsman_id: number;
-	craftsman_name: string;
-	craftsman_image: string;
-	craftsman_know_how: string;
+  craftsman_id: number;
+  craftsman_name: string;
+  craftsman_image: string;
+  craftsman_know_how: string;
 };
