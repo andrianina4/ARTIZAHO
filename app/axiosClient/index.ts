2ad0bcdd2f1ce8326b4066aa88217cc4b841ef4f
Onja <onjanirina1808@gmail.com>
2024-03-18 12:11:32 +0300

    back server

--- app/axiosClient/index.ts
+++ app/axiosClient/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:9237";
 
 const axiosInstanceApiClient = axios.create({
-  baseURL: backendUrl + "/api",
+	baseURL: backendUrl + "/api",
 });
 
-export { axiosInstanceApiClient };
+export {axiosInstanceApiClient};
